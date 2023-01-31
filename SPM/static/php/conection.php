fbfadbd65a14b8274e7f93caf68f287c3e4f60be
Sérgio Pinto (Vimétrica) <sergio.pinto@vimetrica.pt>
2023-01-31 17:20:35 +0000

    php first test

--- SPM/static/php/conection.php
+++ SPM/static/php/conection.php
@@ -1,14 +1,26 @@
 <?php
-
-$user = 'root';
-$password = '';
-$database = 'spm';
-$host = 'localhost';
-
-$mysqli = new mysqli($host,$user,$password,$database);
-
-if($mysqli->error)    
-    die('Connection Failed : ' .$conn->error);
-
-
+class DbConnection{
+ 
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'spm';
+ 
+    protected $connection;
+ 
+    public function __construct(){
+ 
+        if (!isset($this->connection)) {
+ 
+            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+ 
+            if (!$this->connection) {
+                echo 'Cannot connect to database server';
+                exit;
+            }            
+        }    
+ 
+        return $this->connection;
+    }
+}
 ?>
--- SPM/static/php/create_account.php
+++ SPM/static/php/create_account.php
@@ -1,6 +1,5 @@
 <?php
 
-
 session_start();
 
     include('conection.php');
@@ -10,36 +9,37 @@ session_start();
     $password = mysqli_real_escape_string($db,$_POST['password']); 
 
 
+
+    $query = "CREATE TABLE IF NOT EXISTS terapeuta (
+        id int(11) AUTO_INCREMENT,
+        EMAIL varchar(255) NOT NULL,
+        PASSWORD varchar(255) NOT NULL,
+        PRIMARY KEY  (ID)
+        )";
+
+    //$result = mysqli_query($db,$query);
+   
+    if ($conn->query($query) === TRUE) {
+        echo "New table created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    
     $sql = "SELECT id FROM terapeuta WHERE email = '$email' and password = '$password'";
+
+    $sql = "INSERT INTO terapeuta (email,password) VALUES ('$email','$password')";
+    
     if ($conn->query($sql) === TRUE) { 
-        
+        echo "Registration Successfuly...";
+        header("location: ../../html/Account/login.html");
     } else {
         echo "ERROR adding to table.";
     }
 
-
-    //$result = mysqli_query($db,$sql);
-    //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-    //$active = $row['active'];
-
-
-    $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("email");
-         $_SESSION['login_user'] = $email;
-         
-         header("location: ../../index.html");
-
-      }else {
-
-         $error = "Your Login Email or Password is invalid";
-
-      }
-
+    
 ?>
+
 
 
 
